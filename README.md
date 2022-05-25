4fb55fdc3549271d9b39aa30f18975ec27166c00
Nivethaa15 <91970120+Nivethaa15@users.noreply.github.com>
2022-05-25 11:52:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,36 @@
+# Drawing-App
+Drawing App created using d3.js
+
+## Get Started
+Click on this [link](https://abdsamadf.github.io/drawing-app) to see the website **or** you can download or clone the project, and loading up the index.html file in your browser.
+
+## FEATURES
+The drawing app supports:
+
+* Use D3.js
+* Pencil
+* Different colors
+* Different Line weights
+* Eraser
+* Clear board
+* Smooth Lines
+
+![Image of Drawing](drawing.png??raw=true)
+
+## TECHNOLOGY USED
+
+* HTML & CSS
+* D3.js
+* Javascript
+* ES6
+* Material Design Lightweight CSS Framework (MUI)
+* Jquery Awesome Cursor
+
+## TODO
+
+* Save a drawing
+* Custom brush & background
+* Shapes like Rectangle, Line, Dashline, Ellipse
+* Advanced Smoothing
+* Undo/Redo
+
---
+++ js/main.js
@@ -0,0 +1,76 @@
+let color, stroke;
+let activeLine;
+
+const colorElement = document.getElementById('color');
+const strokeElement = document.getElementById('stroke');
+const clear = document.getElementById('clear');
+const eraser = document.getElementById('eraser');
+const svg = d3.select('#svg');
+
+dfltColor = colorElement.options[colorElement.selectedIndex].value;
+dfltStroke = strokeElement.options[strokeElement.selectedIndex].value;
+
+colorElement.onchange = (event) => {
+    color = event.target.value;
+    cursor('pencil', 30, color);
+}
+
+strokeElement.onchange = (event) => {
+    stroke = event.target.value;
+}
+
+clear.onclick = () => {
+    svg.selectAll("*").remove();
+}
+
+eraser.onclick = () => {
+    color = 'white';
+    cursor('eraser', 20);
+}
+
+svg.on('click', click);
+
+function click() {
+    const coords = d3.mouse(this);
+
+    svg.append('circle')
+        .attr('cx', coords[0])
+        .attr('cy', coords[1])
+        .attr('r', stroke ? (stroke / 2) : (dfltStroke / 2))
+        .style('fill', color ? color : dfltColor)
+}
+
+const renderPath = d3.line()
+    .x(d => d[0])
+    .y(d => d[1])
+    .curve(d3.curveBasis)
+
+
+svg.call(d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended));
+
+cursor('pencil', 30);
+
+function dragstarted() {
+    activeLine = svg.append("path").datum([]).attr("class", "line").style('stroke', color ? color : dfltColor).style('stroke-width', stroke ? stroke : dfltStroke);
+    activeLine.datum().push(d3.mouse(this));
+}
+
+function dragged() {
+    activeLine.datum().push(d3.mouse(this));
+    activeLine.attr("d", renderPath);
+}
+
+function dragended() {
+    activeLine = null;
+}
+
+function cursor(icon, size, color = 'black') {
+    $('#svg').awesomeCursor(icon, {
+        color: color,
+        size: size,
+        hotspot: 'center'
+    })
+}
